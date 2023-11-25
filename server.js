6517c065beca770aa61b2f58ff43456748b87de0
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-11-25 18:04:47 +0100

    error handling

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // package imports
+import 'express-async-errors';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
 // files imports
+import errorHandler from './middlewares/errorhandler.js';
 import mongoClient from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
 
 // dotenv config
 dotenv.config();
@@ -17,6 +20,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
+
+app.use('/api/v1/auth', authRoutes);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
