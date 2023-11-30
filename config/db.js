a077a81086cb7dc573dfa04bcd2bedc73272f991
kelvin wooldbridge <kelvinwooldbridge@gmail.com>
2023-11-30 12:25:20 +0100

    updated schema

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const mongoClient = async () => {
   try {
-    const DBURL = process.env.DATABASE_URL || process.env.DATABASE_URL_LOCAL;
+    const DBURL = process.env.MONGODB_URL || process.env.MONGODB_URL_LOCAL;
     const connection = await mongoose.connect(DBURL);
     console.log(`Connected to MongeDb ${mongoose.connection.host}`)
   } catch (err) {
