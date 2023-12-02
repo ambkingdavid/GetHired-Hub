ebc99a941a18532a48c07c078cb5a92a51b942c3
kelvin wooldbridge <kelvinwooldbridge@gmail.com>
2023-12-02 13:14:00 +0100

    adjust conn

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const mongoClient = async () => {
   try {
-    const DBURL = process.env.MONGODB_URL || process.env.MONGODB_URL_LOCAL;
-    const connection = await mongoose.connect(DBURL);
+    const DBURL = process.env.MONGODB_URL;
+    await mongoose.connect(DBURL);
     console.log(`Connected to MongeDb ${mongoose.connection.host}`)
   } catch (err) {
     console.log(`DATABASE connection error: ${err}`);
