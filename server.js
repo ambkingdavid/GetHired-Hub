03b7ca59de11635a92beaf0ddbd401beb67d4ea4
kelvin wooldbridge <kelvinwooldbridge@gmail.com>
2023-12-02 13:14:23 +0100

    add more middlewares

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+//securty packges
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
 // package imports
 import 'express-async-errors';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
+import session from 'express-session'
+import cookieParser from 'cookie-parser';
+import SessionStore from 'connect-mongo';
+import bodyParser from "body-parser";
 // files imports
 import errorHandler from './middlewares/errorhandler.js';
 import mongoClient from './config/db.js';
+// API DOcumenATion
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "swagger-jsdoc";
+// routes
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import jobRoutes from './routes/jobRoutes.js'
@@ -15,17 +26,59 @@ import jobRoutes from './routes/jobRoutes.js'
 dotenv.config();
 
 // connect to Database
-mongoClient();
+await mongoClient();
+
+// Swagger api config
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Get Hired Hub',
+      description: "A hub to land opportunities",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  apis: ['./swaggerDoc/*','./routes/*.js'],
+};
+
+const spec = swaggerDoc(options);
 
 const app = express();
 
+//middelwares
+app.use(helmet());
+app.use(mongoSanitize());
 app.use(express.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  credentials: true,
+}));
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: SessionStore.create({
+      mongoUrl: process.env.SESSION_STORAGE,
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: true,
+    },
+  })
+);
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/job', jobRoutes);
+
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(spec));
 
 app.use(errorHandler);
 
