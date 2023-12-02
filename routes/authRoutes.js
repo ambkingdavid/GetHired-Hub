396ed4ba374f6de124f107ac5ed6b4ac3cc3dc1d
kelvin wooldbridge <kelvinwooldbridge@gmail.com>
2023-12-02 13:19:05 +0100

    add a logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,69 @@
 import { Router } from 'express';
 
 import AuthController from '../controllers/authController.js';
+import limiter from '../utils/limiter.js';
+import userAuth from '../middlewares/authentication.js';
 
 
 const router = Router();
 
-// REGISTER - POST
-router.post('/register', AuthController.register);
+/**
+ *  @swagger
+ *  tags:
+ *    name: Auth
+ *    description: authentication apis
+ */
 
-// LOGIN -POST
-router.post('/login', AuthController.login);
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *    post:
+ *      summary: register new user
+ *      tags: [Auth]
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *      responses:
+ *        200:
+ *          description: Email already registered to a user
+ *        201:
+ *          description: User created successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ *        500:
+ *          description: internal server error
+ */
+router.post('/register', limiter, AuthController.register);
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *  post:
+ *    summary: login page
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: login successfull
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      500:
+ *        description: something went wrong
+ */
+router.post('/login', limiter, AuthController.login);
+
+router.post('/logout', userAuth, AuthController.logout);
 
 export default router;
